0f0d232aa0453d74f4d67e9fc8e373779b344331
nvtrireme <nischay.vaish@triremetrading.com>
2024-12-18 14:59:26 +0530

    rough files removed

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,31 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-verify";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    arthera: {
+      url: process.env.ARTHERA_URL_MAINNET,
+      chainId: 10242,
+      accounts: process.env.PK_ARTHERA_MAINNET !== undefined ? [process.env.PK_ARTHERA_MAINNET] : [],
+    }
+  },
+  sourcify: {
+    enabled: true,
+    apiUrl: "https://sourcify.dev/server",
+    browserUrl: "https://repo.sourcify.dev",
+  }
 };
 
 export default config;
